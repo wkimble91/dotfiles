f5aebcebb61aecc88e36ef8f0addc3b0069357bf
Will Kimble <wkimble91@protonmail.com>
2024-02-14 10:19:16 -0600

    Update polybar_net_speed_down.sh

--- polybar/.config/polybar/scripts/polybar_net_speed_down.sh
+++ polybar/.config/polybar/scripts/polybar_net_speed_down.sh
@@ -9,7 +9,7 @@ net_speed_down() {
 	foreground=%{F#EBDBB2}
 	faded_green=%{F#52CC00}
 	DOWN_NETSPEED=$(awk '{if(l1){printf "%6.1f", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlan0 /proc/net/dev) <(sleep 1; grep wlan0 /proc/net/dev) | tr -d ' ')
-	echo "$faded_green  $foreground $DOWN_NETSPEED KB/s$RESET"
+	echo "$faded_green  $foreground $DOWN_NETSPEED KB/s$RESET"
 }
 
 net_speed_down
